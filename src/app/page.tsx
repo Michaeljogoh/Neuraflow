b8e652d7895bc1a90fbf7b15dd9aa62039ab5be6
unknown <michaeljogoh@gmail.com>
2025-11-12 13:43:04 +0100

    feat: better auth authenication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,15 @@
-
-
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./client";
-import { getQueryClient, trpc } from "@/trpc/server";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
 export default async function Home() {
+  await requireAuth();
 
-  const queryClient = getQueryClient();
+  const data = await caller.getUsers();
 
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
- 
   return (
     <>
       <div className="text-red-500">Hello world</div>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-       <Client />
-      </HydrationBoundary>
+      {JSON.stringify(data)}
     </>
   );
 }
-
-// "use client"
-
-// import { useTRPC } from "@/trpc/client";
-// import { useQuery } from "@tanstack/react-query";
-
-// export default function Home() {
-//     const trpc = useTRPC();
-
-//     const { data: user } = useQuery(trpc.getUsers.queryOptions())
-
-//   return (
-//     <>
-//       <div className="text-red-500">Hello world</div>
-//         {JSON.stringify(user)}
-//     </>
-//   );
-// }
