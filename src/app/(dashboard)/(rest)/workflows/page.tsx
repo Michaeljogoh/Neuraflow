1fcb87381270106a9fd0d67a5980d9a0966ebbc7
unknown <michaeljogoh@gmail.com>
2025-11-20 16:48:00 +0100

    feat: workflow crud

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,11 +1,26 @@
+import { requireAuth } from "@/lib/auth-utils";
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary"
+import { WorkflowsList, WorkflowContainer } from "@/features/workflows/components/workflows";
 
-import { requireAuth } from "@/lib/auth-utils"
+
 
 export default async function Page() {
+  await requireAuth();
 
-  await requireAuth()
-  
+  prefetchWorkflow()
+
   return (
-    <div>Workflow Page</div>
+    <WorkflowContainer>
+    <HydrateClient>
+      <ErrorBoundary fallback={<p>Error!</p>}>
+         <Suspense fallback={<p>Loading...</p>} >
+               <WorkflowsList />
+         </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+    </WorkflowContainer>
   )
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,15 @@
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg'
+import 'dotenv/config'
 
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
-const prisma =   globalForPrisma.prisma ||  new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
 
-if(process.env.NODE_ENV !==  "production"){
-    globalForPrisma.prisma = prisma
-}
+const prisma = new PrismaClient({
+  adapter,
+});
 
 
 export default prisma
+
