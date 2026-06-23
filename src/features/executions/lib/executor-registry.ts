eb0d680dbbb5284a16447dc884960456fd1f4230
Michaeljogoh <michaeljogoh@gmail.com>
2026-06-23 20:35:00 +0100

    feat/discord-and-slack-nodes

--- src/features/executions/lib/executor-registry.ts
+++ src/features/executions/lib/executor-registry.ts
@@ -3,9 +3,11 @@ import { googleFormTriggerExecutor } from "@/features/triggers/component/google-
 import { manualTriggerExecutor } from "@/features/triggers/component/manual-trigger/executor";
 import { stripeTriggerExecutor } from "@/features/triggers/component/stripe-trigger/executor";
 import { anthropicExecutor } from "../components/anthropic/executor";
+import { discordExecutor } from "../components/discord/executor";
 import { geminiExecutor } from "../components/gemini/executor";
 import { httpRequestExecutor } from "../components/http-request/executor";
 import { openAiExecutor } from "../components/openai/executor";
+import { slackExecutor } from "../components/slack/executor";
 import type { NodeExecutor } from "../types";
 
 export const executorRegistry: Record<NodeType, NodeExecutor> = {
@@ -17,6 +19,8 @@ export const executorRegistry: Record<NodeType, NodeExecutor> = {
   [NodeType.GEMINI]: geminiExecutor,
   [NodeType.GOOGLE_FORM_TRIGGER]: googleFormTriggerExecutor,
   [NodeType.STRIPE_TRIGGER]: stripeTriggerExecutor,
+  [NodeType.DISCORD]: discordExecutor,
+  [NodeType.SLACK]: slackExecutor,
 };
 
 export const getExecutor = (type: NodeType): NodeExecutor => {
